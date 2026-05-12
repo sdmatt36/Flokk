3bdc6a87cdb0a550592bb5803d8037ab0da6eb0a
Matt Greene <sdmatt36@gmail.com>
2026-05-12 13:11:00 +0900

    fix: AppHeader passes isLoggedIn prop to AppHeaderClient

--- src/components/ui/AppHeader.tsx
+++ src/components/ui/AppHeader.tsx
@@ -3,11 +3,20 @@ import { AppHeaderClient } from "./AppHeaderClient";
 
 export async function AppHeader() {
   const { userId } = await auth();
-  if (!userId) return null;
 
-  const user = await currentUser();
-  const email = user?.emailAddresses?.[0]?.emailAddress ?? "";
-  const fullName = [user?.firstName, user?.lastName].filter(Boolean).join(" ") || email.split("@")[0] || "User";
+  let fullName = "";
+  let email = "";
 
-  return <AppHeaderClient fullName={fullName} email={email} />;
+  if (userId) {
+    const user = await currentUser();
+    email = user?.emailAddresses?.[0]?.emailAddress ?? "";
+    fullName =
+      [user?.firstName, user?.lastName].filter(Boolean).join(" ") ||
+      email.split("@")[0] ||
+      "User";
+  }
+
+  return (
+    <AppHeaderClient isLoggedIn={!!userId} fullName={fullName} email={email} />
+  );
 }
